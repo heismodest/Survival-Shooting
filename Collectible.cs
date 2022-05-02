41a11dc62e36c527d6f15c39eaa8a9938e5c67bf
heismodest <100833480+heismodest@users.noreply.github.com>
2022-05-02 16:50:04 +0900

    Update Collectible.cs

--- Collectible.cs
+++ Collectible.cs
@@ -6,9 +6,34 @@ Public class Collectible : Monobehaviour
 {
   void OnTriggerEnter(Collider other)
   {
-    if(other.tag == "Player")
-    {
-      Debug.Log("HP up");
+  
+      if(other.tag == "Player")
+        {
+
+        str colName = this.Name;
+
+        switch(colName)
+          {
+          case "HP":
+            Console.WriteLine("Heal 50 point of health";);
+            break;
+          case "SPD":
+            Console.WriteLine("Increased Speed";);
+            break;
+          case "Pwr":
+            Console.WriteLine("atk pwr up for 10 seconds";);
+            break;
+          case "Exp":
+            Console.WriteLine("Exp point up";);
+            break;
+          case "Weapon":
+            Console.WriteLine("Select the Weapon want to upgrade";);
+            break;
+          case "Coin":
+            Console.WriteLine("Coin up";);
+            break;
+          }
+          
       Destroy(this.gameObject);
     {
   {
